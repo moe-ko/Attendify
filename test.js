3ce1a9fb50b2cd99b839088b4c7098441c64f923
Vasavi <vasavi21@gmail.com>
2023-02-28 14:30:07 +0000

    added a new line in the file

---
+++ test.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function test() {
+  return (
+    <View>
+      <Text>testing my first file</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
