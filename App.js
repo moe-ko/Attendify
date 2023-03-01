c9e2f0090e0995d39c042dd75edd48d3c9fdaeb6
Javier Lopez Ramos <javier@brackets.digital>
2023-03-01 21:36:29 +0000

    Get location

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+
+  let text = 'Waiting...';
+  let lat = 0;
+  let long = 0;
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+    lat = JSON.parse(text)['coords']['latitude'];
+    long = JSON.parse(text)['coords']['longitude'];
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>You are here</Text>
+      <Text style={styles.paragraph}>Latitude: {lat}</Text>
+      <Text style={styles.paragraph}>Longitude: {long}</Text>
     </View>
   );
 }
