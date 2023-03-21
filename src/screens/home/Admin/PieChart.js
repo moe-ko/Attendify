d24856cacd927fc427ed7063f70b660d4c8b4949
Javier Lopez Ramos <javier@brackets.digital>
2023-03-21 18:51:02 +0000

    Get Event by date

--- src/screens/home/Admin/PieChart.js
+++ src/screens/home/Admin/PieChart.js
@@ -7,45 +7,58 @@ import AttendanceList from './AttendanceList'
 import { firebase } from '../../../../config'
 
 const PieChart = () => {
-    const [eventId, setEventId] = useState('')
-    const [attend, setAttend] = useState(0)
+    const [eventDate, setEventDate] = useState('')
+    const [attend, setAttend] = useState(100)
     const [absent, setAbsent] = useState(0)
     const [sickLeave, setSickLeave] = useState(0)
     const [annualLeave, setAnnualLeave] = useState(0)
     const [totalAssitance, setTotalAssistance] = useState(0)
     const [viewChart, setViewChart] = useState(false)
 
-    getCurrentEventId = () => {
+    getCurrentEventDate = () => {
         firebase.firestore()
             .collection('events')
-            .orderBy('end', 'asc')
+            .orderBy('end', 'desc')
             .onSnapshot({
                 next: querySnapshot => {
-                    const res = querySnapshot.docs.map(docSnapshot => ({ id: docSnapshot.id }))
-                    if (res.length > 0) (setEventId(res[0]['id']), getTotalAttendance(res[0]['id']))
+                    const res = querySnapshot.docs.map(docSnapshot => ({ date: docSnapshot.data()['end'] }))
+                    if (res.length > 0) (setEventDate(res[0]['date']), getTotalAttendance(res[0]['date']))
                 }
             })
     }
 
-    const getTotalAttendance = (id) => {
+    const getTotalAttendance = (date) => {
         const subscriber = firebase.firestore()
             .collection('events')
-            .doc(id)
-            .onSnapshot(documentSnapshot => {
-                setAttend(documentSnapshot.data()['attendance'].length);
-                setAbsent(documentSnapshot.data()['absent'].length);
-                setSickLeave(documentSnapshot.data()['sick_leave'].length);
-                setAnnualLeave(documentSnapshot.data()['annual_leave'].length);
-                setTotalAssistance(documentSnapshot.data()['attendance'].length + documentSnapshot.data()['absent'].length + documentSnapshot.data()['sick_leave'].length + documentSnapshot.data()['annual_leave'].length);
-                setViewChart(true)
+            .where('end', '==', date)
+            .get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(documentSnapshot => {
+                    setAttend(documentSnapshot.data()['attendance'].length);
+                    setAbsent(documentSnapshot.data()['absent'].length);
+                    setSickLeave(documentSnapshot.data()['sick_leave'].length);
+                    setAnnualLeave(documentSnapshot.data()['annual_leave'].length);
+                    setTotalAssistance(documentSnapshot.data()['attendance'].length + documentSnapshot.data()['absent'].length + documentSnapshot.data()['sick_leave'].length + documentSnapshot.data()['annual_leave'].length);
+                    setViewChart(true)
+                });
             });
-
+        // .onSnapshot(documentSnapshot => {
+        //     setAttend(documentSnapshot.data()['attendance'].length);
+        //     setAbsent(documentSnapshot.data()['absent'].length);
+        //     setSickLeave(documentSnapshot.data()['sick_leave'].length);
+        //     setAnnualLeave(documentSnapshot.data()['annual_leave'].length);
+        //     setTotalAssistance(documentSnapshot.data()['attendance'].length + documentSnapshot.data()['absent'].length + documentSnapshot.data()['sick_leave'].length + documentSnapshot.data()['annual_leave'].length);
+        //     setViewChart(true)
+        // });
         // Stop listening for updates when no longer required
         return () => subscriber();
     }
-    if (eventId == '') {
-        getCurrentEventId()
-    }
+
+    useEffect(() => {
+        getCurrentEventDate()
+    }, [])
+
+
 
     const attendPercent = Math.round(attend / totalAssitance * 100)
     const absentPercent = Math.round(absent / totalAssitance * 100)
