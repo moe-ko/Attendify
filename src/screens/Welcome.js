1bcf54630d7e4e90737a6759f31942c3353184d8
Javier Lopez Ramos <javier@brackets.digital>
2023-04-04 13:48:03 +0100

    Request change password

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -2,39 +2,42 @@ import { View, Text, TouchableOpacity, StyleSheet, Image, ImageBackground } from
 import React from 'react'
 import tailwind from '../constants/tailwind'
 import { ROUTES } from '..'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const Welcome = ({ navigation }) => {
     return (
-        <ImageBackground source={require("../../assets/pic1.png")} className="h-full">
-            <View className="flex-1 justify-center items-center pt-[450]">
-                <View className=" justfy-center items-center" >
-                    <Image source={require('../../assets/Logo.png')} className="w-24 h-24" />
-                </View>
-                <View className={`${tailwind.container}`}>
-                    <Text className="text-center text-black-500 font-semibold mb-10 text-4xl pt-5">
-                        Attendify
-                    </Text>
-                    <View className={`${tailwind.viewWrapper} w-11/12 `}>
-                        <TouchableOpacity
-                            className={`${tailwind.buttonBlue}`}
-                            onPress={() => { navigation.navigate(ROUTES.SIGNIN) }}
-                        >
-                            <Text className={`${tailwind.buttonWhiteText}`}>Sign In</Text>
-                        </TouchableOpacity>
+        <ScrollView>
+            <ImageBackground source={require("../../assets/pic1.png")} className="h-full">
+                <View className="flex-1 justify-center items-center pt-[450]">
+                    <View className=" justfy-center items-center" >
+                        <Image source={require('../../assets/Logo.png')} className="w-24 h-24" />
                     </View>
-                    <View className={`${tailwind.viewWrapper} w-11/12 mb-7`}>
-                        <TouchableOpacity
-                            className={`${tailwind.buttonWhite}`}
-                            onPress={() => { navigation.navigate(ROUTES.SIGNUP) }}>
-                            <Text className={`${tailwind.buttonBlueText}`} >Sign Up</Text>
-                        </TouchableOpacity>
+                    <View className={`${tailwind.container}`}>
+                        <Text className="text-center text-black-500 font-semibold mb-10 text-4xl pt-5">
+                            Attendify
+                        </Text>
+                        <View className={`${tailwind.viewWrapper} w-11/12 `}>
+                            <TouchableOpacity
+                                className={`${tailwind.buttonBlue}`}
+                                onPress={() => { navigation.navigate(ROUTES.SIGNIN) }}
+                            >
+                                <Text className={`${tailwind.buttonWhiteText}`}>Sign In</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View className={`${tailwind.viewWrapper} w-11/12 mb-7`}>
+                            <TouchableOpacity
+                                className={`${tailwind.buttonWhite}`}
+                                onPress={() => { navigation.navigate(ROUTES.SIGNUP) }}>
+                                <Text className={`${tailwind.buttonBlueText}`} >Sign Up</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <Text className="text-center text-[#62ABEF]">
+                            Contact Us
+                        </Text>
                     </View>
-                    <Text className="text-center text-[#62ABEF]">
-                        Contact Us
-                    </Text>
                 </View>
-            </View>
-        </ImageBackground>
+            </ImageBackground>
+        </ScrollView>
     )
 }
 const style = StyleSheet.create({
