22c6a4fa44c30f4f6a9dda75ef724dfa64b2c8a7
Lisett <lisettjosejacob@gmail.com>
2023-04-04 12:47:20 +0100

    tailwind updated

--- src/constants/tailwind.js
+++ src/constants/tailwind.js
@@ -5,7 +5,7 @@ export default {
     titleText: 'font-bold text-3xl',
     slogan: 'font-light text-xl',
     viewWrapper: 'w-full mb-[20]',
-    inputs: 'p-[10] bg-white rounded-2xl',
+    inputs: 'p-[15] bg-white rounded-2xl',
     blueTextLink: 'text-[#62ABEF] font-bold',
     buttonBlue: 'p-[15] b-[7] bg-[#62ABEF] rounded-2xl justify-center',
     buttonBlueText: 'text-[#62ABEF] font-bold text-xl text-center',
