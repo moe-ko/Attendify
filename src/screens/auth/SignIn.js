c44695846a8a0ff4c398b0be561adedce891d1de
Moe <moemyintko@outlook.com>
2023-05-10 11:04:52 +0100

    delete identity

--- src/screens/auth/SignIn.js
+++ src/screens/auth/SignIn.js
@@ -59,12 +59,8 @@ const SignIn = ({ navigation }) => {
                     <View className={`${tailwind.viewWrapper} my-3`}>
                         <TouchableOpacity
                             className={`${tailwind.buttonBlue}`}
-                            //     onPress={() => { signInUser('test@test.com', '123456') }}
-                            // onPress={() => { signInUser('test.test@infosys.com', 'Testtest23') }}
-                            onPress={() => { signInUser('admin@admin.com', 'admin2') }}
-                        // onPress={() => { signInUsers('superadmin@test.com', 'superadmin') }}
-                        // onPress={() => { signInUser(email, password) }}
-                        // disabled={(!email.trim() || !password.trim())}
+                        onPress={() => { signInUser(email, password) }}
+                        disabled={(!email.trim() || !password.trim())}
                         >
                             <Text className={`${tailwind.buttonWhiteText}`}>Sign In</Text>
                         </TouchableOpacity>
